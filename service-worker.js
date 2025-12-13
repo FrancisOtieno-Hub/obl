fe4ee74965caab96d9cc3c1815c959096244c49b
Francis Otieno <info.francisotieno@gmail.com>
2025-12-13 15:22:55 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -30,9 +30,31 @@ const urlsToCache = [
     '/books/When Heaven Walks Our Roads by Francis Otieno.pdf',
     '/books/Wisdom in Managing Resources by Francis Otieno.pdf',
     '/books/Work Less, Earn More by Francis Otieno.pdf',
+    '/covers/100 Best Motivational Quotes Cover Book.jpg',
+    '/covers/Alego Air Cover Book.jpg',
+    '/covers/Avianna Cover Book.jpg',
+    '/covers/Ayanna Cover Book.jpg',
     '/covers/Come As You Are Cover Book.jpg',
+    '/covers/Crowned with Glory Cover Book.jpg',
+    '/covers/Forge Cover Book.jpg',
     '/covers/Leverage Cover Book.jpg',
-    '/covers/The Girl from the Forest Cover Book.jpg'
+    '/covers/Lines on Paper Cover Book.jpg',
+    '/covers/Mastering AI in 2025 Cover Book.jpg',
+    '/covers/Mind Hacking Cover Book.jpg',
+    '/covers/Overcoming Territorial Spirits Cover Book.jpg',
+    '/covers/Raising Stars Cover Book.jpg',
+    '/covers/The Africa We Must Build Cover Book.jpg',
+    '/covers/The Art of a Lasting Union Cover Book.jpg',
+    '/covers/The Child I Had to Raise Cover Book.jpg',
+    '/covers/The Everyday Investor Cover Book.jpg',
+    '/covers/The Girl from the Forest Cover Book.jpg',
+    '/covers/The Nine-Month Journey Cover Book.jpg',
+    '/covers/The Principle of Generosity Cover Book.jpg',
+    '/covers/The Quiet Strength Cover Book.jpg',
+    '/covers/Unshakable Cover Book.jpg',
+    '/covers/When Heaven Walks Our Roads Cover Book.jpg',
+    '/covers/Wisdom in Managing Resources Cover Book.jpg',
+    '/covers/Work Less, Earn More Cover Book.jpg'
 ];
 
 // Install SW and cache assets
@@ -48,5 +70,6 @@ self.addEventListener('fetch', event => {
         caches.match(event.request).then(response => response || fetch(event.request))
     );
 });
+
 
 
